ce06627f5272b31b6bbac365b932b82e44716d99
olegsugatov <oleg.sugatov@gmail.com>
2017-04-01 02:23:51 +0300

    dev-server config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
 
 module.exports = {
 	entry: './src/app.js',
 	output: {
-		path: __dirname + '/dist',
+		path: path.resolve(__dirname, 'dist'),
 		filename: 'app.bundle.js'
 	},
 	module: {
@@ -17,6 +18,12 @@ module.exports = {
 				})
 			}
 	 	]
+	},
+	devServer: {
+		contentBase: path.join(__dirname, "dist"),
+  		compress: true,
+  		stats: 'errors-only',
+  		open: true
 	},
 	plugins: [ 
 		new HtmlWebpackPlugin({
