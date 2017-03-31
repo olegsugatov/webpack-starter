bc39e39c23919603068ff5a07f0fde6a62183114
olegsugatov <oleg.sugatov@gmail.com>
2017-03-31 18:46:18 +0300

    html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
 	entry: './src/app.js',
 	output: {
-		filename: './dist/app.bundle.js'
-	}
+		path: __dirname + '/dist',
+		filename: 'app.bundle.js'
+	},
+	plugins: [ 
+		new HtmlWebpackPlugin({
+    		title: 'Project Demo',
+    		minify: {
+    			collapseWhitespace: true
+    		},
+    		hash: true,
+    		template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
+  		})
+	]
 }
